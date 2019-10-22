242e3c1429fc00231e9ca3f2568dc55ed1b2cb80
tomocy <tomocy.dev@gmail.com>
2019-10-22 23:12:42 +0900

    add (Verb).String
    (Verb).String generate a string of Verb

--- deferr.go
+++ deferr.go
@@ -2,6 +2,8 @@ package deferr
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
 
 func Format(err *error, format string, as ...interface{}) {
@@ -22,3 +24,31 @@ type Verb struct {
 	flag, width, prec int
 	verb              rune
 }
+
+func (v Verb) String() string {
+	var w strings.Builder
+	fmt.Fprint(&w, "%")
+	if isFlag(v.flag) {
+		fmt.Fprint(&w, string([]rune{rune(v.flag)}))
+	}
+	if v.width != 0 {
+		fmt.Fprint(&w, v.width)
+	}
+	if v.prec != 0 {
+		fmt.Fprint(&w, ".", v.prec)
+	}
+	fmt.Fprint(&w, string([]rune{v.verb}))
+
+	return w.String()
+}
+
+func isFlag(f int) bool {
+	sort.Ints(flags)
+	i := sort.Search(len(flags), func(i int) bool {
+		return f <= flags[i]
+	})
+
+	return i < len(flags) && flags[i] == f
+}
+
+var flags = []int{' ', '#', '+', '-', '0'}
